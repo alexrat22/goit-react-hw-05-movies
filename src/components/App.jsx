4e536c62d1a32924c0024dbb2924c3afadf7cf93
Alexander Ratushnyi <olexandr.ratushnyi@gmail.com>
2023-06-21 11:00:34 +0300

    create pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Movies from './pages/Movies';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <header>
+        <nav>
+          <Link to="./">Home</Link>
+          <Link to="./movies">Movies</Link>
+        </nav>
+      </header>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+      </Routes>
+    </>
   );
 };
---
+++ src/services/api.jsx
@@ -0,0 +1,2 @@
+//const API_KEY = '265f78c728b685275d7bdd6274612da2';
+//const BASE_URL = 'https://api.themoviedb.org/3/';
